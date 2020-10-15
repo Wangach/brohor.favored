07b00241150654ed22b5a3af4a069f468b431fbd
John Kimani <jwangach@gmail.com>
2020-10-15 06:30:45 +0300

    error solved

--- admin/fifa/scripts/database.php
+++ admin/fifa/scripts/database.php
@@ -1,5 +1,5 @@
 <?php
-$initialize = mysqli_connect('localhost', 'root', '','newtings');
+$initialize = mysqli_connect('localhost', 'root', '','systwone');
 if (!$initialize) {
 	echo "Connection to the Database has not been successful!".mysqli_error($initialize);
 }
--- sql/looserp.sql
+++ sql/looserp.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `systwone`.`lmatches` ( `Hplayer` VARCHAR(1000) NOT NULL , `Aplayer` VARCHAR(1000) NOT NULL , `Hteam` VARCHAR(1000) NOT NULL , `Ateam` VARCHAR(1000) NOT NULL , `Hscore` INT NOT NULL , `Ascore` INT NOT NULL , `winner` VARCHAR(1000) NOT NULL , `looser` VARCHAR(1000) NOT NULL , `tomatch` VARCHAR(100) NOT NULL , `tarehe` VARCHAR(100) NOT NULL , `wakati` VARCHAR(100) NOT NULL , `debt` INT NOT NULL , `matchid` VARCHAR(1000) NOT NULL ) ENGINE = InnoDB;
+/*CREATE TABLE `systwone`.`lmatches` ( `Hplayer` VARCHAR(1000) NOT NULL , `Aplayer` VARCHAR(1000) NOT NULL , `Hteam` VARCHAR(1000) NOT NULL , `Ateam` VARCHAR(1000) NOT NULL , `Hscore` INT NOT NULL , `Ascore` INT NOT NULL , `winner` VARCHAR(1000) NOT NULL , `looser` VARCHAR(1000) NOT NULL , `tomatch` VARCHAR(100) NOT NULL , `tarehe` VARCHAR(100) NOT NULL , `wakati` VARCHAR(100) NOT NULL , `debt` INT NOT NULL , `matchid` VARCHAR(1000) NOT NULL ) ENGINE = InnoDB;*/
 
 
 
