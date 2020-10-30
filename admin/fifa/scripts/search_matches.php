fe0765f81562afcc47b50c1efd3bb91874798d42
John <jwangach@gmail.com>
2020-10-30 20:07:37 +0300

    progress

--- admin/fifa/scripts/search_matches.php
+++ admin/fifa/scripts/search_matches.php
@@ -11,14 +11,24 @@ $seachTerm = mysqli_real_escape_string($initialize, $_POST['nop']);
 switch ($searchType) {
 	//won matches
 	case 'wonmat':
-		# code...
+		$mySearch = "SELECT * FROM lmatches WHERE winner = '$seachTerm'";
+		$realSer = mysqli_query($initialize, $mySearch);
+
+		//If there's data found
+		if (mysqli_num_rows($realSer) > 0) {
+			while ($data = mysqli_fetch_assoc($realSer)) {
+				
+			}
+		}
 		break;
 	//lost matches
 	case 'losmat':
 		# code...
 		break;
-	//Bot Won And Lost
-	
+	//Both Won And Lost
+	case 'allmat':
+		# code...
+		break;
 	default:
 		# code...
 		break;
