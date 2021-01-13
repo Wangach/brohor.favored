d97bda5c1a8c3b1f96f9fe24e7722bab740d9daa
John <jwangach@gmail.com>
2021-01-13 19:49:15 +0300

    Movie half way

--- admin/index.php
+++ admin/index.php
@@ -205,36 +205,9 @@ if (!isset($_SESSION['administrator']) || empty($_SESSION['administrator'])) {
 							<h3 class="text-center text-info text-muted">Recent Movies</h3>
 						</div>
 						<div class="col-md-12">
-							<table class="table table-striped">
-								<thead>
-								<tr>
-								  <th scope="col">#</th>
-								  <th scope="col">First</th>
-								  <th scope="col">Last</th>
-								  <th scope="col">Handle</th>
-								</tr>
-								</thead>
-								<tbody>
-								<tr>
-								  <th scope="row">1</th>
-								  <td>Mark</td>
-								  <td>Otto</td>
-								  <td>@mdo</td>
-								</tr>
-								<tr>
-								  <th scope="row">2</th>
-								  <td>Jacob</td>
-								  <td>Thornton</td>
-								  <td>@fat</td>
-								</tr>
-								<tr>
-								  <th scope="row">3</th>
-								  <td>Larry</td>
-								  <td>the Bird</td>
-								  <td>@twitter</td>
-								</tr>
-								</tbody>
-							</table>
+							<div class="recent-movies">
+								<?php include 'movies/moviescr/latestmovies.php'; ?>
+							</div><!--/.recent-movies-->
 						</div><!--/col-md-12/-->
 					</div><!--/.nested row/-->
 				</div>
