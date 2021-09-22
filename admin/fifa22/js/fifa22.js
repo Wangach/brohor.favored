8123822b46be35068ba54d9122cd409bd99a3764
John Kimani <jwangach@gmail.com>
2021-09-22 19:19:46 +0300

    cards

---
+++ admin/fifa22/js/fifa22.js
@@ -0,0 +1,12 @@
+window.onload = () =>{
+   //alert(`Hello`);Get Button
+   let menuBar = document.querySelector('#sidebar-btn');
+   let sidebar = document.querySelector('.f2-sidebar');
+   menuBar.addEventListener('click', () =>{
+       sidebar.classList.toggle('return');
+    //    linkspans.classList.toggle('hide');
+    //    linkicons.classList.add('show-ic');
+   })
+
+}
+ 
